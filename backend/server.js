21c129b7de8998f5039ffa0be5487eca329b32b9
lapulga237 <dieudonneonana30@gmail.com>
2026-04-13 13:04:39 +0100

    added environment variables

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,15 @@ const historyRoutes = require('./routes/historyRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
 
 // Connect to MongoDB (local instance)
-mongoose.connect('mongodb+srv://dieudonneonana30_db_user:jg39kpTkzhD536M6@cluster0.oih1jpt.mongodb.net/?appName=Cluster0', {
+mongoose.connect(process.env.Mongo_URI, {
    
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 app.use('/api/history', historyRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
