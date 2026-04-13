5e70d12b3c4a945e4c7888da2243cf7a04bd0dcd
lapulga237 <dieudonneonana30@gmail.com>
2026-04-13 14:16:47 +0100

    corrected env vars name

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ env }) => {
-  const siteUrl = env.VITE_SITE_URL || 'http://localhost:5173'
-  const backendPort = env.VITE_BACKEND_PORT || '5000'
+  const siteUrl = import.meta.env.VITE_SITE_URL || 'http://localhost:5173'
+  const backendPort = import.meta.env.VITE_BACKEND_PORT || '5000'
 
   return {
     plugins: [react()],
